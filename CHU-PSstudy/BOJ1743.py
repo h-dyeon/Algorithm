bc0aed6b133de0f7b0906330952dba4ba9b02b0e
h-dyeon <nochedeuyuni@cau.ac.kr>
2021-06-14 17:02:55 +0900

    BOJ 1743 solved

--- CHU-PSstudy/BOJ1743.py
+++ CHU-PSstudy/BOJ1743.py
@@ -2,14 +2,25 @@ from sys import stdin
 import sys
 
 N,M,K = map(int, stdin.readline().strip().split(' ')) #row, coulmn, submerged cell
-
-matrix = [[0]*(M+1) for _ in range(M+1)]
-
-for i in range(K):
-    r,c=map(int,stdin.readline().strip())
-    matrix[r][c]=1
+mapinfo = [list(map(int,sys.stdin.readline().strip().split(' '))) for _ in range(K)]
 
 dr=[0,-1,0,1]
 dc=[1,0,-1,0]
 
-print(3)
+maxx=0
+while mapinfo:
+    now=mapinfo.pop()
+    value=1
+    queue=[now]
+    while queue:
+        tmp=queue.pop()
+        for i in range(4):
+            newR=tmp[0]+dr[i]
+            newC=tmp[1]+dc[i]
+            if [newR,newC] in mapinfo:
+                mapinfo.remove([newR,newC])
+                queue.append([newR,newC])
+                value+=1
+    maxx=max(maxx,value)
+
+print(maxx)
