bf290c14e835227d54079eb55f14fc6f2be6ee99
h-dyeon <hdyeon@gmail.com>
2022-02-09 22:24:01 +0900

    solve again boj 1992

--- BOJ/BOJ1992.py
+++ BOJ/BOJ1992.py
@@ -1,28 +1,65 @@
+# 20220209 again!
+
 import sys
-# N=8
-# matrix=[[1, 1, 1, 1, 0, 0, 0, 0], [1, 1, 1, 1, 0, 0, 0, 0], [0, 0, 0, 1, 1, 1, 0, 0], [0, 0, 0, 1, 1, 1, 0, 0], [1, 1, 1, 1, 0, 0, 0, 0], [1, 1, 1, 1, 0, 0, 0, 0], [1, 1, 1, 1, 0, 0, 1, 1], [1, 1, 1, 1, 0, 0, 1, 1]]
-# N=4
-# matrix=[[1,1,1,1],[1,1,1,1],[0,0,0,1],[0,0,0,1]]
+N=int(input())
+arr=[list(map(int,sys.stdin.readline().strip())) for _ in range(N)]
 
-def quadTree(r, c, size):
-    if size==1:
-        return str(matrix[r][c])
-    
+def dq(sr,sc,l):
     tmp=0
-    for i in range(size):
-        tmp+=sum(matrix[r+i][c:c+size])
-    
-    if tmp/(size*size) == 1:
-        return str(1)
-    elif tmp/(size*size) ==0:
-        return str(0)
-    else :
-        half=int(size/2)
-        return ("(" + quadTree(r,c,half) + 
-                    quadTree(r,c+half,half) + 
-                    quadTree(r+half,c,half) + 
-                    quadTree(r+half,c+half,half) + ")" )
+    for i in range(l):
+        tmp+=sum(arr[sr+i][sc:sc+l])
+    if tmp==0: return "0"
+    elif tmp==l*l: return "1"
+    else:
+        return ("("+dq(sr,sc,int(l/2))+
+                    dq(sr,sc+int(l/2),int(l/2))+
+                    dq(sr+int(l/2),sc,int(l/2))+
+                    dq(sr+int(l/2),sc+int(l/2),int(l/2))+
+                    ")")
 
-N=int(sys.stdin.readline().strip())
-matrix=[list(map(int,sys.stdin.readline().strip())) for _ in range(N)]
-print(quadTree(0,0,N))
+print(dq(0,0,N))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# import sys
+# # N=8
+# # matrix=[[1, 1, 1, 1, 0, 0, 0, 0], [1, 1, 1, 1, 0, 0, 0, 0], [0, 0, 0, 1, 1, 1, 0, 0], [0, 0, 0, 1, 1, 1, 0, 0], [1, 1, 1, 1, 0, 0, 0, 0], [1, 1, 1, 1, 0, 0, 0, 0], [1, 1, 1, 1, 0, 0, 1, 1], [1, 1, 1, 1, 0, 0, 1, 1]]
+# # N=4
+# # matrix=[[1,1,1,1],[1,1,1,1],[0,0,0,1],[0,0,0,1]]
+
+# def quadTree(r, c, size):
+#     if size==1:
+#         return str(matrix[r][c])
+    
+#     tmp=0
+#     for i in range(size):
+#         tmp+=sum(matrix[r+i][c:c+size])
+    
+#     if tmp/(size*size) == 1:
+#         return str(1)
+#     elif tmp/(size*size) ==0:
+#         return str(0)
+#     else :
+#         half=int(size/2)
+#         return ("(" + quadTree(r,c,half) + 
+#                     quadTree(r,c+half,half) + 
+#                     quadTree(r+half,c,half) + 
+#                     quadTree(r+half,c+half,half) + ")" )
+
+# N=int(sys.stdin.readline().strip())
+# matrix=[list(map(int,sys.stdin.readline().strip())) for _ in range(N)]
+# print(quadTree(0,0,N))
