dbd62acdf5bcd2e121d3fce6cb0c7852055350dc
h-dyeon <hdyeon@gmail.com>
2022-04-06 00:16:25 +0900

    re-solve boj 14502, 14503

--- BOJ/BOJ14502.py
+++ BOJ/BOJ14502.py
@@ -1,70 +1,115 @@
-import sys
-import copy
 from collections import deque
-
-# N,M=list(map(int,sys.stdin.readline().strip().split(' ')))
-# arr=[list(map(int,sys.stdin.readline().strip().split(' '))) for _ in range(N)]
-# print(arr)
-N=4
-M=6
-arr=[[0, 0, 0, 0, 0, 0], [1, 0, 0, 0, 0, 2], [1, 1, 1, 0, 0, 2], [0, 0, 0, 0, 0, 2]]
-dr=[1,0,-1,0]
-dc=[0,1,0,-1]
-
-def checkvirus(a,b,c):
-    narr=copy.deepcopy(arr)
-    # narr[int(a/M)][a%M]=1
-    # narr[int(b/M)][b%M]=1
-    # narr[int(c/M)][c%M]=1
-    narr[a[0]][a[1]]=1
-    narr[b[0]][b[1]]=1
-    narr[c[0]][c[1]]=1
-
-    visited=[[False]*M for _ in range(N)]
-    for i in range(N):
-        for j in range(M):
-            if narr[i][j]==2 and visited[i][j]==False:
-                dq=deque([(i,j)])
-                visited[i][j]=True
-                while dq:
-                    x,y=dq.popleft()
-                    for k in range(4):
-                        nx=x+dr[k]
-                        ny=y+dc[k]
-                        if 0<=nx<N and 0<=ny<M and visited[nx][ny]==False and narr[nx][ny]==0:
-                            narr[nx][ny]=2
-                            visited[nx][ny]=True
-                            dq.append((nx,ny))
-    
-    return sum([narr[row].count(0) for row in range(N)])
-    # ans=0
-    # for i in range(N):
-    #     for j in range(M):
-    #         if narr[i][j]==0:
-    #             ans+=1
-    # return ans
-
+N,M=map(int,input().split(' '))
+arr=[list(map(int,input().split(' '))) for _ in range(N)]
 
 answer=0
-case=[(i, j) for i in range(N) for j in range(M) if arr[i][j]==0]
-for i in range(len(case)-2):
-    for j in range(i+1, len(case)-1):
-        for k in range(j+1, len(case)):
-            A, B, C = case[i], case[j], case[k]
-            answer=max(answer,checkvirus(A,B,C))
+
+def bfs():
+    global answer
+    visited=[row[:] for row in arr]
+    for i in range(N):
+        for j in range(M):
+            if visited[i][j]==2: #virus
+                dq=deque([(i,j)])
+                visited[i][j]=3
+                while dq:
+                    r,c=dq.popleft()
+                    for dr,dc in [(0,1),(0,-1),(1,0),(-1,0)]:
+                        nr,nc=r+dr,c+dc
+                        if 0<=nr<N and 0<=nc<M and (visited[nr][nc]==2 or visited[nr][nc]==0):
+                            visited[nr][nc]=3
+                            dq.append((nr,nc))
+    s=sum([r.count(0) for r in visited])
+    answer=max(answer,s)
+
+for i in range(N*M-2):
+    if arr[int(i/M)][i%M]!=0: continue
+    for j in range(i+1,N*M-1):
+        if arr[int(j/M)][j%M]!=0: continue
+        for k in range(j+1,N*M):
+            if arr[int(k/M)][k%M]!=0: continue
+            # print(i,j,k)
+            arr[int(i/M)][i%M]=1
+            arr[int(j/M)][j%M]=1
+            arr[int(k/M)][k%M]=1
+            bfs()
+            arr[int(i/M)][i%M]=0
+            arr[int(j/M)][j%M]=0
+            arr[int(k/M)][k%M]=0
+
 print(answer)
 
+
+
+
+########################################before #############3
+# import sys
+# import copy
+# from collections import deque
+
+# # N,M=list(map(int,sys.stdin.readline().strip().split(' ')))
+# # arr=[list(map(int,sys.stdin.readline().strip().split(' '))) for _ in range(N)]
+# # print(arr)
+# N=4
+# M=6
+# arr=[[0, 0, 0, 0, 0, 0], [1, 0, 0, 0, 0, 2], [1, 1, 1, 0, 0, 2], [0, 0, 0, 0, 0, 2]]
+# dr=[1,0,-1,0]
+# dc=[0,1,0,-1]
+
+# def checkvirus(a,b,c):
+#     narr=copy.deepcopy(arr)
+#     # narr[int(a/M)][a%M]=1
+#     # narr[int(b/M)][b%M]=1
+#     # narr[int(c/M)][c%M]=1
+#     narr[a[0]][a[1]]=1
+#     narr[b[0]][b[1]]=1
+#     narr[c[0]][c[1]]=1
+
+#     visited=[[False]*M for _ in range(N)]
+#     for i in range(N):
+#         for j in range(M):
+#             if narr[i][j]==2 and visited[i][j]==False:
+#                 dq=deque([(i,j)])
+#                 visited[i][j]=True
+#                 while dq:
+#                     x,y=dq.popleft()
+#                     for k in range(4):
+#                         nx=x+dr[k]
+#                         ny=y+dc[k]
+#                         if 0<=nx<N and 0<=ny<M and visited[nx][ny]==False and narr[nx][ny]==0:
+#                             narr[nx][ny]=2
+#                             visited[nx][ny]=True
+#                             dq.append((nx,ny))
+    
+#     return sum([narr[row].count(0) for row in range(N)])
+#     # ans=0
+#     # for i in range(N):
+#     #     for j in range(M):
+#     #         if narr[i][j]==0:
+#     #             ans+=1
+#     # return ans
+
+
 # answer=0
-# for i in range(N*M-2):
-#     ir,ic=int(i/M),i%M
-#     if arr[ir][ic]!=0: continue
-#     for j in range(i+1,N*M-1):
-#         jr,jc=int(j/M),j%M
-#         if arr[jr][jc]!=0: continue
-#         for k in range(j+1,N*M):
-#             kr,kc=int(k/M),k%M
-#             if arr[kr][kc]!=0: continue
-#             answer=max(answer,checkvirus(i,j,k))
+# case=[(i, j) for i in range(N) for j in range(M) if arr[i][j]==0]
+# for i in range(len(case)-2):
+#     for j in range(i+1, len(case)-1):
+#         for k in range(j+1, len(case)):
+#             A, B, C = case[i], case[j], case[k]
+#             answer=max(answer,checkvirus(A,B,C))
 # print(answer)
+
+# # answer=0
+# # for i in range(N*M-2):
+# #     ir,ic=int(i/M),i%M
+# #     if arr[ir][ic]!=0: continue
+# #     for j in range(i+1,N*M-1):
+# #         jr,jc=int(j/M),j%M
+# #         if arr[jr][jc]!=0: continue
+# #         for k in range(j+1,N*M):
+# #             kr,kc=int(k/M),k%M
+# #             if arr[kr][kc]!=0: continue
+# #             answer=max(answer,checkvirus(i,j,k))
+# # print(answer)
 
             
--- BOJ/BOJ14503.py
+++ BOJ/BOJ14503.py
@@ -1,47 +1,81 @@
-import sys
+N,M=map(int,input().split(' '))
+r,c,d=map(int,input().split(' '))
+arr=[list(map(int,input().split(' '))) for _ in range(N)]
+print(arr)
 
-# N,M=list(map(int,sys.stdin.readline().strip().split(' ')))
-# r,c,d=list(map(int,sys.stdin.readline().strip().split(' ')))
-# arr=[list(map(int,sys.stdin.readline().strip().split(' '))) for _ in range(N)]
-# print(arr)
-
-N,M=3,3
-r,c,d=1,1,0
-arr=[[1, 1, 1], [1, 0, 1], [1, 1, 1]]
-
-# N,M=11,10
-# r,c,d=7,4,0
-# arr=[[1, 1, 1, 1, 1, 1, 1, 1, 1, 1], [1, 0, 0, 0, 0, 0, 0, 0, 0, 1], [1, 0, 0, 0, 1, 1, 1, 1, 0, 1], [1, 0, 0, 1, 1, 0, 0, 0, 0, 1], [1, 0, 1, 1, 0, 0, 0, 0, 0, 1], [1, 0, 0, 0, 0, 0, 0, 0, 0, 1], [1, 0, 0, 0, 0, 0, 0, 1, 0, 1], [1, 0, 0, 0, 0, 0, 1, 1, 0, 1], [1, 0, 0, 0, 0, 0, 1, 1, 0, 1], [1, 0, 0, 0, 0, 0, 0, 0, 0, 1], [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]]
-
-dr=[-1,0,1,0]
-dc=[0,1,0,-1]
-
+direc=[[-1,0],[0,1],[1,0],[0,-1]]
 answer=0
-status=True
-for _ in range(10000000):
-    if status: # number 1
-        answer+=1
+while True:
+    # step 1
+    if arr[r][c]==0:
         arr[r][c]=2 #clean
-        status=False # goto number2
-    else: # number2
-        isCleaned=False
-        for i in range(1,5):
-            nd=(d+4-i)%4
-            nr,nc=r+dr[nd],c+dc[nd]
-            if 0<=nr<N and 0<=nc<M and arr[nr][nc]==0:
-                r,c,d=nr,nc,nd
-                isCleaned=True
-                break
-        if isCleaned:
-            status=True # goto number1
-            continue
-        elif arr[r+dr[(d+4-2)%4]][c+dc[(d+4-2)%4]]!=1: #there is blocked 
-            r=r+dr[(d+4-2)%4]
-            c=c+dc[(d+4-2)%4]
-            continue
-        else:
+        answer+=1
+    
+    #step 2
+    status=False
+    for i in range(1,5):
+        nr,nc=r+direc[(d-i+4)%4][0],c+direc[(d-i+4)%4][1]
+        if arr[nr][nc]==0:
+            r,c=nr,nc
+            d=(d-i+4)%4
+            status=True
             break
+    if status:
+        continue
+    else:
+        nr,nc=r+direc[(d+2)%4][0], c+direc[(d+2)%4][1]
+        if arr[nr][nc]==1:
+            break
+        else:
+            r,c=nr,nc
+
 print(answer)
+
+
+# import sys
+
+# # N,M=list(map(int,sys.stdin.readline().strip().split(' ')))
+# # r,c,d=list(map(int,sys.stdin.readline().strip().split(' ')))
+# # arr=[list(map(int,sys.stdin.readline().strip().split(' '))) for _ in range(N)]
+# # print(arr)
+
+# N,M=3,3
+# r,c,d=1,1,0
+# arr=[[1, 1, 1], [1, 0, 1], [1, 1, 1]]
+
+# # N,M=11,10
+# # r,c,d=7,4,0
+# # arr=[[1, 1, 1, 1, 1, 1, 1, 1, 1, 1], [1, 0, 0, 0, 0, 0, 0, 0, 0, 1], [1, 0, 0, 0, 1, 1, 1, 1, 0, 1], [1, 0, 0, 1, 1, 0, 0, 0, 0, 1], [1, 0, 1, 1, 0, 0, 0, 0, 0, 1], [1, 0, 0, 0, 0, 0, 0, 0, 0, 1], [1, 0, 0, 0, 0, 0, 0, 1, 0, 1], [1, 0, 0, 0, 0, 0, 1, 1, 0, 1], [1, 0, 0, 0, 0, 0, 1, 1, 0, 1], [1, 0, 0, 0, 0, 0, 0, 0, 0, 1], [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]]
+
+# dr=[-1,0,1,0]
+# dc=[0,1,0,-1]
+
+# answer=0
+# status=True
+# for _ in range(10000000):
+#     if status: # number 1
+#         answer+=1
+#         arr[r][c]=2 #clean
+#         status=False # goto number2
+#     else: # number2
+#         isCleaned=False
+#         for i in range(1,5):
+#             nd=(d+4-i)%4
+#             nr,nc=r+dr[nd],c+dc[nd]
+#             if 0<=nr<N and 0<=nc<M and arr[nr][nc]==0:
+#                 r,c,d=nr,nc,nd
+#                 isCleaned=True
+#                 break
+#         if isCleaned:
+#             status=True # goto number1
+#             continue
+#         elif arr[r+dr[(d+4-2)%4]][c+dc[(d+4-2)%4]]!=1: #there is blocked 
+#             r=r+dr[(d+4-2)%4]
+#             c=c+dc[(d+4-2)%4]
+#             continue
+#         else:
+#             break
+# print(answer)
 
 
 
