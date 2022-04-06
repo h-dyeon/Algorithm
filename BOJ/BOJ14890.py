15c05f0e55cd9dde30b29b8fca7d48b03e125dc5
h-dyeon <hdyeon@gmail.com>
2022-04-06 16:42:12 +0900

    re-solve boj 14890, 14891

--- BOJ/BOJ14890.py
+++ BOJ/BOJ14890.py
@@ -1,109 +1,167 @@
-import sys
+########## 22.04.06
+N,L=list(map(int,input().split(' ')))
+arr=[list(map(int,input().split(' '))) for _ in range(N)]
+print(arr)
 
-# N,L=list(map(int,sys.stdin.readline().strip().split()))
-# arr=[list(map(int,sys.stdin.readline().strip().split())) for _ in range(N)]
-# print(arr)
-
-N,L=6,2
-arr=[[3,2,1,1,2,3], [3, 2, 2, 1, 2, 3], [3, 2, 2, 2, 3, 3], [3, 3, 3, 3, 3, 3], [3,3, 3, 3, 2, 2], [3,3, 3, 3, 2, 2]]
-# N,L=6,1
-# arr=[[3, 2, 1, 1, 2, 3], [3, 2, 2, 1, 2, 3], [3, 2, 2, 2, 3, 3], [3, 3, 3, 3, 3, 3], [3, 3, 3, 3, 2, 2], [3, 3, 3, 3, 2, 2]]
-
-# N,L=6,2
-# arr=[[3, 3, 3, 3, 3, 3], [2, 3, 3, 3, 3, 3], [2, 2, 2, 3, 2, 3], [1, 1, 1, 2, 2, 2], [1, 1, 1, 3, 3, 1], [1, 1, 2, 3, 3, 2]]
-
-# N,L=6,2
-# arr=[[3,2,2,2,3,3], [2, 3, 3, 3, 3, 3], [2, 2, 2, 3, 2, 3], [1, 1, 1, 2, 2, 2], [1, 1, 1, 3, 3, 1], [1, 1, 2, 3, 3, 2]]
-
-def rowCheck(r,cs,cf,step):
-    cnt,num=1,arr[r][cs]
-    for c in range(cs,cf,step):
-        if cnt<0 and arr[r][c]-arr[r][c+step]!=0:
-            return False
-        elif arr[r][c]-arr[r][c+step]==0:
+def rowcheck(r):
+    cnt=1
+    for i in range(N-1):
+        if arr[r][i]==arr[r][i+1]:
             cnt+=1
-        elif abs(arr[r][c]-arr[r][c+step])>1:
-            return False
-        elif arr[r][c]-arr[r][c+step]==1: #down
-            num=arr[r][c+step]
-            cnt=-1*L+1
-        elif arr[r][c]-arr[r][c+step]==-1: #up
-            if L<=cnt:
-                num=arr[r][c+step]
+        elif cnt<0:
+            return 0
+        elif abs(arr[r][i]-arr[r][i+1])>1:
+            return 0
+        elif arr[r][i]>arr[r][i+1]:
+            cnt=-1*L+1        
+        else:
+            if cnt>=L:
                 cnt=1
             else:
-                return False
+                return 0
     if cnt<0:
-        return False
-    return True
+        return 0
+    return 1
 
-def colCheck(c,rs,rf,step):
-    cnt,num=1,arr[rs][c]
-    for r in range(rs,rf,step):
-        if cnt<0 and arr[r][c]-arr[r+step][c]!=0:
-            return False
-        elif arr[r][c]-arr[r+step][c]==0:
+
+def colcheck(c):
+    cnt=1
+    for i in range(N-1):
+        if arr[i][c]==arr[i+1][c]:
             cnt+=1
-        elif abs(arr[r][c]-arr[r+step][c])>1:
-            return False
-        elif arr[r][c]-arr[r+step][c]==1: #down
-            num=arr[r+step][c]
-            cnt=-1*L+1
-        elif arr[r][c]-arr[r+step][c]==-1: #up
-            if L<=cnt:
-                num=arr[r+step][c]
+        elif cnt<0:
+            return 0
+        elif abs(arr[i][c]-arr[i+1][c])>1:
+            return 0
+        elif arr[i][c]>arr[i+1][c]:
+            cnt=-1*L+1        
+        else:
+            if cnt>=L:
                 cnt=1
             else:
-                return False
+                return 0
     if cnt<0:
-        return False
-    return True
+        return 0
+    return 1
 
 
 answer=0
 for i in range(N):
-    print("-----------",i)
-    print(rowCheck(i,0,N-1,1),rowCheck(i,N-1,0,-1))
-    if rowCheck(i,0,N-1,1) or rowCheck(i,N-1,0,-1):
-        answer+=1
-    print(colCheck(i,0,N-1,1) , colCheck(i,N-1,0,-1))
-    if colCheck(i,0,N-1,1) or colCheck(i,N-1,0,-1):
-        answer+=1
-
+    answer+=rowcheck(i)
+    answer+=colcheck(i)
 print(answer)
 
 
 
+
+
+###############22.03.26########
+# import sys
+
+# # N,L=list(map(int,sys.stdin.readline().strip().split()))
+# # arr=[list(map(int,sys.stdin.readline().strip().split())) for _ in range(N)]
+# # print(arr)
+
+# N,L=6,2
+# arr=[[3,2,1,1,2,3], [3, 2, 2, 1, 2, 3], [3, 2, 2, 2, 3, 3], [3, 3, 3, 3, 3, 3], [3,3, 3, 3, 2, 2], [3,3, 3, 3, 2, 2]]
+# # N,L=6,1
+# # arr=[[3, 2, 1, 1, 2, 3], [3, 2, 2, 1, 2, 3], [3, 2, 2, 2, 3, 3], [3, 3, 3, 3, 3, 3], [3, 3, 3, 3, 2, 2], [3, 3, 3, 3, 2, 2]]
+
+# # N,L=6,2
+# # arr=[[3, 3, 3, 3, 3, 3], [2, 3, 3, 3, 3, 3], [2, 2, 2, 3, 2, 3], [1, 1, 1, 2, 2, 2], [1, 1, 1, 3, 3, 1], [1, 1, 2, 3, 3, 2]]
+
+# # N,L=6,2
+# # arr=[[3,2,2,2,3,3], [2, 3, 3, 3, 3, 3], [2, 2, 2, 3, 2, 3], [1, 1, 1, 2, 2, 2], [1, 1, 1, 3, 3, 1], [1, 1, 2, 3, 3, 2]]
+
 # def rowCheck(r,cs,cf,step):
 #     cnt,num=1,arr[r][cs]
 #     for c in range(cs,cf,step):
-#         if arr[r][c]-arr[r][c+step]==0:
+#         if cnt<0 and arr[r][c]-arr[r][c+step]!=0:
+#             return False
+#         elif arr[r][c]-arr[r][c+step]==0:
 #             cnt+=1
-#             continue
 #         elif abs(arr[r][c]-arr[r][c+step])>1:
 #             return False
-#         elif abs(arr[r][c]-arr[r][c+step])==1:
-#             if L<=cnt and arr[r][c]<arr[r][c+step]:
+#         elif arr[r][c]-arr[r][c+step]==1: #down
+#             num=arr[r][c+step]
+#             cnt=-1*L+1
+#         elif arr[r][c]-arr[r][c+step]==-1: #up
+#             if L<=cnt:
 #                 num=arr[r][c+step]
 #                 cnt=1
-#                 continue
 #             else:
 #                 return False
+#     if cnt<0:
+#         return False
 #     return True
 
 # def colCheck(c,rs,rf,step):
 #     cnt,num=1,arr[rs][c]
 #     for r in range(rs,rf,step):
-#         if arr[r][c]-arr[r+step][c]==0:
+#         if cnt<0 and arr[r][c]-arr[r+step][c]!=0:
+#             return False
+#         elif arr[r][c]-arr[r+step][c]==0:
 #             cnt+=1
-#             continue
 #         elif abs(arr[r][c]-arr[r+step][c])>1:
 #             return False
-#         elif abs(arr[r][c]-arr[r+step][c])==1:
-#             if L<=cnt and arr[r][c]<arr[r+step][c]:
+#         elif arr[r][c]-arr[r+step][c]==1: #down
+#             num=arr[r+step][c]
+#             cnt=-1*L+1
+#         elif arr[r][c]-arr[r+step][c]==-1: #up
+#             if L<=cnt:
 #                 num=arr[r+step][c]
 #                 cnt=1
-#                 continue
 #             else:
 #                 return False
+#     if cnt<0:
+#         return False
 #     return True
+
+
+# answer=0
+# for i in range(N):
+#     print("-----------",i)
+#     print(rowCheck(i,0,N-1,1),rowCheck(i,N-1,0,-1))
+#     if rowCheck(i,0,N-1,1) or rowCheck(i,N-1,0,-1):
+#         answer+=1
+#     print(colCheck(i,0,N-1,1) , colCheck(i,N-1,0,-1))
+#     if colCheck(i,0,N-1,1) or colCheck(i,N-1,0,-1):
+#         answer+=1
+
+# print(answer)
+
+
+
+# # def rowCheck(r,cs,cf,step):
+# #     cnt,num=1,arr[r][cs]
+# #     for c in range(cs,cf,step):
+# #         if arr[r][c]-arr[r][c+step]==0:
+# #             cnt+=1
+# #             continue
+# #         elif abs(arr[r][c]-arr[r][c+step])>1:
+# #             return False
+# #         elif abs(arr[r][c]-arr[r][c+step])==1:
+# #             if L<=cnt and arr[r][c]<arr[r][c+step]:
+# #                 num=arr[r][c+step]
+# #                 cnt=1
+# #                 continue
+# #             else:
+# #                 return False
+# #     return True
+
+# # def colCheck(c,rs,rf,step):
+# #     cnt,num=1,arr[rs][c]
+# #     for r in range(rs,rf,step):
+# #         if arr[r][c]-arr[r+step][c]==0:
+# #             cnt+=1
+# #             continue
+# #         elif abs(arr[r][c]-arr[r+step][c])>1:
+# #             return False
+# #         elif abs(arr[r][c]-arr[r+step][c])==1:
+# #             if L<=cnt and arr[r][c]<arr[r+step][c]:
+# #                 num=arr[r+step][c]
+# #                 cnt=1
+# #                 continue
+# #             else:
+# #                 return False
+# #     return True
--- BOJ/BOJ14891.py
+++ BOJ/BOJ14891.py
@@ -1,46 +1,100 @@
-import sys
-from collections import deque
-
-arr=[deque([]) for _ in range(4)]
+##############22.04.06
+arr=[[0]*8 for _ in range(4)]
 for i in range(4):
-    tmp=sys.stdin.readline().strip() #.split(' ')
+    tmp=input()
     for j in range(8):
-        arr[i].append(tmp[j])
-# arr=[sys.stdin.readline().strip().split(' ') for _ in range(4)]
-#print(arr)
+        arr[i][j]=tmp[j]
+
+
+def isrotate(tob,direc,rot):
+    if tob>0 and rot[tob-1]==0:
+        if arr[tob][6]!=arr[tob-1][2]:
+            rot[tob-1]=1 if direc==-1 else -1
+            isrotate(tob-1,rot[tob-1],rot)
+    if tob<3 and rot[tob+1]==0:
+        if arr[tob][2]!=arr[tob+1][6]:
+            rot[tob+1]=1 if direc==-1 else -1
+            isrotate(tob+1,rot[tob+1],rot)
+    return rot
+
 K=int(input())
-
-# arr=[deque(['1', '0', '1', '0', '1', '1', '1', '1']), deque(['0', '1', '1', '1', '1', '1', '0', '1']), deque(['1', '1', '0', '0', '1', '1', '1', '0']), deque(['0', '0', '0', '0', '0', '0', '1', '0'])]
-# K=2
-# hello=[(3,-1),(1,1)]
-
-for k in range(K):
-    num,direc=list(map(int,sys.stdin.readline().strip().split(' ')))
-# for k in range(K):
-#     num,direc=hello[k]
-
-    #find rotate direction
+for _ in range(K):
+    tob,direc=map(int,input().split(' '))
     rot=[0,0,0,0]
-    rot[num-1]=direc
-    dq=deque([(num-1,direc)])
-    while dq:
-        x,d=dq.popleft()
-        for dx in [-1,1]:
-            nx=x+dx
-            if 0<=nx<4 and rot[nx]==0:
-                if dx==-1 and arr[x][6]!=arr[nx][2]: #left
-                    rot[nx]=-1*d
-                    dq.append((nx,-1*d))
-                elif dx==1 and arr[x][2]!=arr[nx][6]: #right
-                    rot[nx]=-1*d
-                    dq.append((nx,-1*d))
-    
-    #rotate
-    for i in range(4):
-        arr[i].rotate(rot[i])
+    rot[tob-1]=direc
+    rot=isrotate(tob-1,direc,rot)
 
-#calculate score
+    for i in range(4):
+        if rot[i]==1:
+            arr[i]=arr[i][-1:]+arr[i][:-1]
+        elif rot[i]==-1:
+            arr[i]=arr[i][1:]+arr[i][:1]
+            
+
 answer=0
 for i in range(4):
-    answer+=int(arr[i][0])*(2**i) #if N=0, S=1
+    if int(arr[i][0])==1:
+        answer+=2**i
 print(answer)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#################22.03.28????###
+# import sys
+# from collections import deque
+
+# arr=[deque([]) for _ in range(4)]
+# for i in range(4):
+#     tmp=sys.stdin.readline().strip() #.split(' ')
+#     for j in range(8):
+#         arr[i].append(tmp[j])
+# # arr=[sys.stdin.readline().strip().split(' ') for _ in range(4)]
+# #print(arr)
+# K=int(input())
+
+# # arr=[deque(['1', '0', '1', '0', '1', '1', '1', '1']), deque(['0', '1', '1', '1', '1', '1', '0', '1']), deque(['1', '1', '0', '0', '1', '1', '1', '0']), deque(['0', '0', '0', '0', '0', '0', '1', '0'])]
+# # K=2
+# # hello=[(3,-1),(1,1)]
+
+# for k in range(K):
+#     num,direc=list(map(int,sys.stdin.readline().strip().split(' ')))
+# # for k in range(K):
+# #     num,direc=hello[k]
+
+#     #find rotate direction
+#     rot=[0,0,0,0]
+#     rot[num-1]=direc
+#     dq=deque([(num-1,direc)])
+#     while dq:
+#         x,d=dq.popleft()
+#         for dx in [-1,1]:
+#             nx=x+dx
+#             if 0<=nx<4 and rot[nx]==0:
+#                 if dx==-1 and arr[x][6]!=arr[nx][2]: #left
+#                     rot[nx]=-1*d
+#                     dq.append((nx,-1*d))
+#                 elif dx==1 and arr[x][2]!=arr[nx][6]: #right
+#                     rot[nx]=-1*d
+#                     dq.append((nx,-1*d))
+    
+#     #rotate
+#     for i in range(4):
+#         arr[i].rotate(rot[i])
+
+# #calculate score
+# answer=0
+# for i in range(4):
+#     answer+=int(arr[i][0])*(2**i) #if N=0, S=1
+# print(answer)
