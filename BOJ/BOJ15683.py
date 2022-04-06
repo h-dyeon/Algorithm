cd5fe2c5239e53d6a11d76ae9eae7a042b37cee4
h-dyeon <hdyeon@gmail.com>
2022-04-07 00:19:59 +0900

    re-solve boj 15683, 15684

--- BOJ/BOJ15683.py
+++ BOJ/BOJ15683.py
@@ -1,144 +1,126 @@
-# from collections import deque
 
-# N,M,H=map(int,input().split(' '))
-# arr=[[0]*(N-1) for _ in range(H)]
-# for _ in range(M):
-#     a,b=map(int,input().split(' '))
-#     arr[a-1][b-1]=1
+##################################################22.04.06
+
+N,M=map(int,input().split(' '))
+arr=[list(map(int,input().split(' '))) for _ in range(N)]
+
+cctv=[]
+for i in range(N):
+    for j in range(M):
+        if 1<=arr[i][j]<=5:
+            cctv.append((i,j))
+
+dr=[0,1,0,-1]
+dc=[1,0,-1,0]
+cctvDirec=[[],[[0],[1],[2],[3]],
+        [[0,2],[1,3]],
+        [[0,1],[1,2],[2,3],[3,0]],
+        [[0,1,2],[1,2,3],[2,3,0],[3,0,1]],
+        [[0,1,2,3]]]
+answer=sum([r.count(0) for r in arr])
+
+def check(matrix,d,r,c):
+    while True:
+        nr,nc=r+dr[d],c+dc[d]
+        if 0>nr or 0>nc or N<=nr or M<=nc or matrix[nr][nc]==6:
+            break
+        matrix[nr][nc]=-1
+        r,c=nr,nc
+    return
+
+
+def dfs(cnt,narr):
+    global answer
+    if cnt==len(cctv):
+        answer=min(answer,sum([r.count(0) for r in narr]))
+        return
+    
+    r,c=cctv[cnt]
+    ttype=arr[r][c]
+    directions=cctvDirec[ttype]
+    for a in directions:
+        matrix=[row[:] for row in narr]
+        for b in a:
+            check(matrix,b,r,c)
+        dfs(cnt+1,matrix)
+
+dfs(0,arr)
+print(answer)
+
+
+
+
+
+##################################################3
+# import sys
+
+
+# N,M=map(int,input().split(' '))
+# arr=[list(map(int,input().split(' '))) for _ in range(N)]
 # print(arr)
 
-# # N,M,H=5,5,6
-# # arr=[[1, 0, 0, 0], [0, 0, 1, 0], [0, 1, 0, 0], [0, 0, 0, 0], [1, 0, 0, 1], [0, 0, 0, 0]]
-# def check():
-#     for c in range(N-1):
-#         tmp=c
-#         for r in range(H):
-#             if tmp<N-1 and arr[r][tmp]==1:
-#                 tmp+=1
-#             elif tmp>0 and arr[r][tmp-1]==1:
-#                 tmp-=1
-#         if c!=tmp:
-#             return False   
-#     return True
+# # N,M=4,6
+# # arr=[[0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0], [0, 0, 1, 0, 6, 0], [0, 0, 0, 0, 0, 0]]
+# dr=[1,0,-1,0]
+# dc=[0,1,0,-1]
+# answer=100000000000 #min
 
-# def checkside(r,c):
-#     if arr[r][c]==1: 
-#         return True
-#     if c>0 and arr[r][c-1]!=0:
-#         return True
-#     if c<N-2 and arr[r][c+1]!=0:
-#         return True
-#     return False
+# #find cctv
+# cctv=[]
+# for i in range(N):
+#     for j in range(M):
+#         if 0<arr[i][j]<6:
+#             cctv.append((i,j,arr[i][j]))
+
+# def going(copyarr,direc,r,c):
+#     for k in range(1,1000):
+#         nr=r+k*dr[direc]
+#         nc=c+k*dc[direc]
+#         if 0<=nr<N and 0<=nc<M and copyarr[nr][nc]!=6:
+#             copyarr[nr][nc]=99
+#         else:
+#             break
+#     return copyarr
 
 
-# def findanswer():
-#     # add 0
-#     if check():
-#         return 0
+# def check(copyarr,idx):
+#     global answer
+#     tmp=sum([copyarr[i].count(0) for i in range(N)])
+#     if tmp==0 or len(cctv)==idx:  
+#         answer=min(answer,tmp)
+#         return
 
-#     # add 1
-#     for i in range(0,(N-1)*H):
-#         r,c=int(i/(N-1)),i%(N-1)
-#         if checkside(r,c):
-#             continue
-#         arr[r][c]=1
-#         if check():
-#             return 1
-#         arr[r][c]=0
-        
-#     # add 2
-#     for i in range(0,(N-1)*H-1):
-#         r,c=int(i/(N-1)),i%(N-1)
-#         if checkside(r,c):
-#             continue
-#         arr[r][c]=1
-#         for j in range(i,(N-1)*H):
-#             rr,cc=int(j/(N-1)),j%(N-1)
-#             if checkside(rr,cc):
-#                 continue
-#             arr[rr][cc]=1
-#             if check():
-#                 return 2
-#             arr[rr][cc]=0
-#         arr[r][c]=0
+#     r,c,t=cctv[idx]
     
-#     # add 3
-#     for i in range(0,(N-1)*H-2):
-#         r,c=int(i/(N-1)),i%(N-1)
-#         if checkside(r,c):
-#             continue
-#         arr[r][c]=1
-#         for j in range(i,(N-1)*H-1):
-#             rr,cc=int(j/(N-1)),j%(N-1)
-#             if checkside(rr,cc):
-#                 continue
-#             arr[rr][cc]=1
-#             for k in range(j,(N-1)*H-2):
-#                 rrr,ccc=int(k/(N-1)),k%(N-1)
-#                 if checkside(rrr,ccc):
-#                     continue
-#                 arr[rrr][ccc]=1
-#                 if check():
-#                     return 3
-#                 arr[rrr][ccc]=0
-#             arr[rr][cc]=0
-#         arr[r][c]=0
-            
-#     return -1
-            
+#     if t==1:
+#         for d in range(4):
+#             narr=[r[:] for r in copyarr]
+#             check(going(narr,d,r,c),idx+1)
+#     elif t==2:
+#         for i in range(2):
+#             narr=[r[:] for r in copyarr]
+#             going(narr,i,r,c)
+#             going(narr,i+2,r,c)
+#             check(narr,idx+1)
+#     elif t==3:
+#         for i in range(4):
+#             narr=[r[:] for r in copyarr]
+#             going(narr,i,r,c)
+#             going(narr,(i+1)%4,r,c)
+#             check(narr,idx+1)
+#     elif t==4:
+#         for i in range(4):
+#             narr=[r[:] for r in copyarr]
+#             going(narr,i,r,c)
+#             going(narr,(i+1)%4,r,c)
+#             going(narr,(i+2)%4,r,c)
+#             check(narr,idx+1)
+#     elif t==5:
+#         narr=[r[:] for r in copyarr]
+#         for d in range(4):
+#             going(narr,d,r,c)
+#         check(narr,idx+1)
 
-# print(findanswer())
-
-
-
-
-N,M,H=map(int,input().split(' '))
-arr=[[0]*(N-1) for _ in range(H)]
-for _ in range(M):
-    a,b=map(int,input().split(' '))
-    arr[a-1][b-1]=1
-print(arr)
-
-# N,M,H=5,5,6
-# arr=[[1, 0, 0, 0], [0, 0, 1, 0], [0, 1, 0, 0], [0, 0, 0, 0], [1, 0, 0, 1], [0, 0, 0, 0]]
-def check():
-    for c in range(N-1):
-        tmp=c
-        for r in range(H):
-            if tmp<N-1 and arr[r][tmp]==1:
-                tmp+=1
-            elif tmp>0 and arr[r][tmp-1]==1:
-                tmp-=1
-        if c!=tmp:
-            return False   
-    return True
-
-def dfs(cnt,idxfrom):
-    global answer
-    if cnt>3:
-        answer=min(answer,1e9)
-        return
-    if cnt>=answer:
-        return
-    if check():
-        answer=min(answer,cnt)
-        return
-
-    for i in range(idxfrom+1,(N-1)*H): #-(2-cnt)):
-        c=i%(N-1)
-        r=int(i/(N-1))
-        if arr[r][c]==1:
-            continue
-        if c>0 and arr[r][c-1]!=0:
-            continue
-        if c<N-2 and arr[r][c+1]!=0:
-            continue
-        # print("="*2*(cnt+5), cnt, idxfrom,",,,,,,,",i)
-        arr[r][c]=1
-        dfs(cnt+1,i)
-        arr[r][c]=0
-
-
-answer=1e9
-dfs(0,-1)
-print(answer if answer!=1e9 else -1)
+# copyarr=[r[:] for r in arr]
+# check(copyarr,0)
+# print(answer)
--- BOJ/BOJ15684.py
+++ BOJ/BOJ15684.py
@@ -1,72 +1,212 @@
-import sys
 
 
-N,M=map(int,input().split(' '))
-arr=[list(map(int,input().split(' '))) for _ in range(N)]
+
+
+N,M,H=map(int,input().split(' '))
+arr=[[0]*(N-1) for _ in range(H)]
+for _ in range(M):
+    a,b=map(int,input().split(' '))
+    arr[a-1][b-1]=1
 print(arr)
 
-# N,M=4,6
-# arr=[[0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0], [0, 0, 1, 0, 6, 0], [0, 0, 0, 0, 0, 0]]
-dr=[1,0,-1,0]
-dc=[0,1,0,-1]
-answer=100000000000 #min
+# N,M,H=5,5,6
+# arr=[[1, 0, 0, 0], [0, 0, 1, 0], [0, 1, 0, 0], [0, 0, 0, 0], [1, 0, 0, 1], [0, 0, 0, 0]]
+answer=1e9
 
-#find cctv
-cctv=[]
-for i in range(N):
-    for j in range(M):
-        if 0<arr[i][j]<6:
-            cctv.append((i,j,arr[i][j]))
-
-def going(copyarr,direc,r,c):
-    for k in range(1,1000):
-        nr=r+k*dr[direc]
-        nc=c+k*dc[direc]
-        if 0<=nr<N and 0<=nc<M and copyarr[nr][nc]!=6:
-            copyarr[nr][nc]=99
-        else:
-            break
-    return copyarr
+def check():
+    for c in range(N):
+        i=c
+        for r in range(H):
+            if i<N-1 and arr[r][i]==1:
+                i+=1
+                continue
+            if i>0 and arr[r][i-1]==1:
+                i-=1
+                continue
+        if c!=i:
+            return False
+    return True
 
 
-def check(copyarr,idx):
-    global answer
-    tmp=sum([copyarr[i].count(0) for i in range(N)])
-    if tmp==0 or len(cctv)==idx:  
-        answer=min(answer,tmp)
+def pick(cnt,startidx):
+    global arr,answer
+    if check():
+        answer=min(answer,cnt)
+        return
+    if cnt>=answer:
+        return
+    if cnt==3:
         return
 
-    r,c,t=cctv[idx]
-    
-    if t==1:
-        for d in range(4):
-            narr=[r[:] for r in copyarr]
-            check(going(narr,d,r,c),idx+1)
-    elif t==2:
-        for i in range(2):
-            narr=[r[:] for r in copyarr]
-            going(narr,i,r,c)
-            going(narr,i+2,r,c)
-            check(narr,idx+1)
-    elif t==3:
-        for i in range(4):
-            narr=[r[:] for r in copyarr]
-            going(narr,i,r,c)
-            going(narr,(i+1)%4,r,c)
-            check(narr,idx+1)
-    elif t==4:
-        for i in range(4):
-            narr=[r[:] for r in copyarr]
-            going(narr,i,r,c)
-            going(narr,(i+1)%4,r,c)
-            going(narr,(i+2)%4,r,c)
-            check(narr,idx+1)
-    elif t==5:
-        narr=[r[:] for r in copyarr]
-        for d in range(4):
-            going(narr,d,r,c)
-        check(narr,idx+1)
+    for i in range(startidx,(N-1)*H):
+        x=int(i/(N-1))
+        y=i%(N-1)
+        if arr[x][y]==1:
+            continue
+        if y-1>=0 and arr[x][y-1]==1:
+            continue
+        if y+1<N-1 and arr[x][y+1]==1:
+            continue
 
-copyarr=[r[:] for r in arr]
-check(copyarr,0)
-print(answer)
+        arr[x][y]=1
+        pick(cnt+1,i+1)
+        arr[x][y]=0
+
+
+
+pick(0,0)
+if answer==1e9:
+    print(-1)
+else:
+    print(answer)
+
+
+
+
+
+########################################3
+
+# from collections import deque
+
+# N,M,H=map(int,input().split(' '))
+# arr=[[0]*(N-1) for _ in range(H)]
+# for _ in range(M):
+#     a,b=map(int,input().split(' '))
+#     arr[a-1][b-1]=1
+# print(arr)
+
+# # N,M,H=5,5,6
+# # arr=[[1, 0, 0, 0], [0, 0, 1, 0], [0, 1, 0, 0], [0, 0, 0, 0], [1, 0, 0, 1], [0, 0, 0, 0]]
+# def check():
+#     for c in range(N-1):
+#         tmp=c
+#         for r in range(H):
+#             if tmp<N-1 and arr[r][tmp]==1:
+#                 tmp+=1
+#             elif tmp>0 and arr[r][tmp-1]==1:
+#                 tmp-=1
+#         if c!=tmp:
+#             return False   
+#     return True
+
+# def checkside(r,c):
+#     if arr[r][c]==1: 
+#         return True
+#     if c>0 and arr[r][c-1]!=0:
+#         return True
+#     if c<N-2 and arr[r][c+1]!=0:
+#         return True
+#     return False
+
+
+# def findanswer():
+#     # add 0
+#     if check():
+#         return 0
+
+#     # add 1
+#     for i in range(0,(N-1)*H):
+#         r,c=int(i/(N-1)),i%(N-1)
+#         if checkside(r,c):
+#             continue
+#         arr[r][c]=1
+#         if check():
+#             return 1
+#         arr[r][c]=0
+        
+#     # add 2
+#     for i in range(0,(N-1)*H-1):
+#         r,c=int(i/(N-1)),i%(N-1)
+#         if checkside(r,c):
+#             continue
+#         arr[r][c]=1
+#         for j in range(i,(N-1)*H):
+#             rr,cc=int(j/(N-1)),j%(N-1)
+#             if checkside(rr,cc):
+#                 continue
+#             arr[rr][cc]=1
+#             if check():
+#                 return 2
+#             arr[rr][cc]=0
+#         arr[r][c]=0
+    
+#     # add 3
+#     for i in range(0,(N-1)*H-2):
+#         r,c=int(i/(N-1)),i%(N-1)
+#         if checkside(r,c):
+#             continue
+#         arr[r][c]=1
+#         for j in range(i,(N-1)*H-1):
+#             rr,cc=int(j/(N-1)),j%(N-1)
+#             if checkside(rr,cc):
+#                 continue
+#             arr[rr][cc]=1
+#             for k in range(j,(N-1)*H-2):
+#                 rrr,ccc=int(k/(N-1)),k%(N-1)
+#                 if checkside(rrr,ccc):
+#                     continue
+#                 arr[rrr][ccc]=1
+#                 if check():
+#                     return 3
+#                 arr[rrr][ccc]=0
+#             arr[rr][cc]=0
+#         arr[r][c]=0
+            
+#     return -1
+            
+
+# print(findanswer())
+
+
+
+
+N,M,H=map(int,input().split(' '))
+arr=[[0]*(N-1) for _ in range(H)]
+for _ in range(M):
+    a,b=map(int,input().split(' '))
+    arr[a-1][b-1]=1
+print(arr)
+
+# N,M,H=5,5,6
+# arr=[[1, 0, 0, 0], [0, 0, 1, 0], [0, 1, 0, 0], [0, 0, 0, 0], [1, 0, 0, 1], [0, 0, 0, 0]]
+def check():
+    for c in range(N-1):
+        tmp=c
+        for r in range(H):
+            if tmp<N-1 and arr[r][tmp]==1:
+                tmp+=1
+            elif tmp>0 and arr[r][tmp-1]==1:
+                tmp-=1
+        if c!=tmp:
+            return False   
+    return True
+
+def dfs(cnt,idxfrom):
+    global answer
+    if cnt>3:
+        answer=min(answer,1e9)
+        return
+    if cnt>=answer:
+        return
+    if check():
+        answer=min(answer,cnt)
+        return
+
+    for i in range(idxfrom+1,(N-1)*H): #-(2-cnt)):
+        c=i%(N-1)
+        r=int(i/(N-1))
+        if arr[r][c]==1:
+            continue
+        if c>0 and arr[r][c-1]!=0:
+            continue
+        if c<N-2 and arr[r][c+1]!=0:
+            continue
+        # print("="*2*(cnt+5), cnt, idxfrom,",,,,,,,",i)
+        arr[r][c]=1
+        dfs(cnt+1,i)
+        arr[r][c]=0
+
+
+answer=1e9
+dfs(0,-1)
+print(answer if answer!=1e9 else -1)
